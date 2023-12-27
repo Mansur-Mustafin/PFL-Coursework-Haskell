4a42ce4945153c908b6e5745e17d95106d31df4b
Mansur <mustafin.mansur02@gmail.com>
2023-12-27 19:06:11 +0000

    added version with height as argument

--- src/Map.hs
+++ src/Map.hs
@@ -1,5 +1,7 @@
 module Map (Map, empty, insert, find, map2List, isEmpty) where 
 
+import Data.List (permutations)
+
 -- TODO add garbage collection
 data Map k v = Empty | Node (k,v) (Map k v) (Map k v) Int deriving Show
 
@@ -16,15 +18,15 @@ isEmpty _ = False
 
 -- Insert or update element in a tree
 insert :: (Ord k) => k -> v -> Map k v -> Map k v
-insert k v Empty = Node (k,v) Empty Empty 0
-insert k v (Node (k1,v1) l r bf)
+insert k v Empty = Node (k,v) Empty Empty 1
+insert k v (Node (k1,v1) l r h)
  | k > k1 = let newRight = insert k v r
-                newBf = (height l) - (height newRight)
-            in balance $ Node (k1,v1) l newRight newBf
+                newHeightR = updateHeight (Node (k1,v1) l newRight 0)
+            in balance $ Node (k1,v1) l newRight newHeightR
  | k < k1 = let newLeft = insert k v l
-                newBf = (height newLeft) - (height r)
-            in balance $ Node (k1,v1) newLeft r newBf
- | otherwise = Node (k,v) l r bf
+                newHeight = updateHeight (Node (k1,v1) newLeft r 0)
+            in balance $ Node (k1,v1) newLeft r newHeight
+ | otherwise = Node (k,v) l r h
 
 
 -- Find element in a tree 
@@ -39,62 +41,84 @@ find k (Node (k1,v1) l r _)
 -- Inorder Traversal
 map2List :: Map k v -> [(k,v)]
 map2List Empty = []
-map2List (Node (k,v) esq dir bf) = map2List esq ++ [(k,v)] ++ map2List dir
+map2List (Node (k,v) esq dir _) = map2List esq ++ [(k,v)] ++ map2List dir
 
 
 -- Calculate the height of a tree
 height :: Map k v -> Int 
 height Empty = 0
-height (Node _ esq dir bf) = 1 + max (height esq) (height dir)
+height (Node _ Empty Empty _) = 1
+height (Node _ _ _ h) = h
+
+height' :: Map k v -> Int 
+height' Empty = 0
+height' (Node _ Empty Empty _) = 1
+height' (Node _ l r _) = 1 + max (height' l) (height' r)
+
+updateHeight :: Map k v -> Int 
+updateHeight Empty = 0
+updateHeight (Node _ Empty Empty _) = 1
+updateHeight (Node _ l r _) = 1 + max (height l) (height r)
 
 
 -- Check if a tree is balanced
 isBalanced :: Map k v -> Bool
 isBalanced Empty = True 
-isBalanced (Node (k,v) esq dir bf) 
+isBalanced (Node (k,v) esq dir h) 
  | not (isBalanced esq && isBalanced dir) = False
+ | abs ((height' esq) - (height' dir)) > 1 = False
  | abs ((height esq) - (height dir)) > 1 = False
  | otherwise = True 
 
 
 -- Get left subtree 
 leftSubTree :: Map k v -> Map k v
-leftSubTree (Node (k,v) esq dir bf) = esq
+leftSubTree (Node (k,v) esq _ _) = esq
 
 
 -- Get right subtree 
 rightSubTree :: Map k v -> Map k v
-rightSubTree (Node (k,v) esq dir bf) = dir 
+rightSubTree (Node (k,v) _ dir _) = dir 
 
 
--- Get Balance Factor
-getBf :: Map k v -> Int 
-getBf (Node _ _ _ bf) = bf
+balanceFactor :: Map k v -> Int
+balanceFactor Empty = 0
+balanceFactor (Node _ l r _) = (height l) - (height r)
 
 
 -- Left Left Case 
 llRotation :: Map k v -> Map k v
-llRotation (Node (zk, zv) (Node (yk, yv) x t3 ybf) t4 zbf) =
-    Node (yk, yv) x (Node (zk, zv) t3 t4 (zbf - 2)) (ybf - 1)
+llRotation (Node (zk, zv) (Node (yk, yv) x t3 yh) t4 zh) =
+    let newZh = 1 + max (height t3) (height t4)
+        newYh = 1 + max (height x) newZh
+    in Node (yk, yv) x (Node (zk, zv) t3 t4 newZh) newYh
 llRotation _ = error "LL rotation cannot be applied."
 
 
 -- Right Right Case 
 rrRotation :: Map k v -> Map k v
-rrRotation (Node (zk, zv) t1 (Node (yk, yv) t2 x ybf) zbf) =
-    Node (yk, yv) (Node (zk, zv) t1 t2 (zbf + 2)) x (ybf + 1)
+rrRotation (Node (zk, zv) t1 (Node (yk, yv) t2 x yh) zh) =
+    let newZh = 1 + max (height t1) (height t2)
+        newYh = 1 + max (height x) newZh
+    in Node (yk, yv) (Node (zk, zv) t1 t2 newZh) x newYh
 rrRotation _ = error "RR rotation cannot be applied."
 
 
 -- Balance a tree
 balance :: Map k v -> Map k v 
-balance (Node (k, v) esq dir bf)
- | bf == 2  && (getBf esq) == 1  = llRotation (Node (k, v) esq dir bf)
- | bf == -2 && (getBf dir) == -1 = rrRotation (Node (k, v) esq dir bf)
- | bf == 2  && (getBf esq) == -1 = llRotation $ Node (k, v) (rrRotation esq) dir bf
- | bf == -2 && (getBf dir) == 1  = rrRotation $ Node (k, v) esq (llRotation dir) bf
- | otherwise = (Node (k, v) esq dir bf)
-
+balance (Node (k, v) esq dir h)
+ | bf == 2  && (balanceFactor esq) == 1  = llRotation (Node (k, v) esq dir h)
+ | bf == -2 && (balanceFactor dir) == -1 = rrRotation (Node (k, v) esq dir h)
+ | bf == 2  && (balanceFactor esq) == -1 = let 
+                                            rrEsq = (rrRotation esq)
+                                            newHight = updateHeight (Node (k, v) rrEsq dir 0)
+                                           in llRotation $ Node (k, v) rrEsq dir newHight
+ | bf == -2 && (balanceFactor dir) == 1  = let 
+                                            rrDir = llRotation dir
+                                            newHight = updateHeight (Node (k, v) esq rrDir 0)
+                                           in rrRotation $ Node (k, v) esq rrDir newHight
+ | otherwise = (Node (k, v) esq dir h)
+ where bf = balanceFactor (Node (k, v) esq dir h)
 
 -- ####################################################################################################################
 -- #                                                                                                                  #
@@ -102,58 +126,20 @@ balance (Node (k, v) esq dir bf)
 -- #                                                                                                                  #
 -- ####################################################################################################################
 
-createTree :: [(Int, String)] -> Map Int String
-createTree = foldl (flip $ uncurry insert) empty
 
--- A larger test tree
-largerTestTree :: Map Int String
-largerTestTree = createTree [(10, "j"), (20, "k"), (30, "l"), (40, "m"), (50, "n"), (60, "o"), (70, "p"), (80, "q")]
+insertIntoTree :: [Int] -> Map Int Int
+insertIntoTree = foldl (\t x -> insert x x t) empty
 
-muchLargerTestTree :: Map Int String
-muchLargerTestTree = createTree [
-    (10, "A"), (20, "B"), (30, "C"), (40, "D"),
-    (250, "Y"), (260, "Z"), (270, "AA"), (280, "BB"),
-    (50, "E"), (310, "EE"), (70, "G"), (80, "H"),
-    (210, "U"), (220, "V"), (230, "W"), (240, "X"),
-    (90, "I"), (100, "J"), (110, "K"), (120, "L"),
-    (130, "M"), (140, "N"), (150, "O"), (160, "P"),
-    (170, "Q"), (180, "R"), (190, "S"), (200, "T"),
-    (290, "CC"), (300, "DD"), (60, "F"), (320, "FF")
-    ]
+checkBalance :: [Int] -> IO ()
+checkBalance permutation = 
+    if isBalanced (insertIntoTree permutation) 
+    then return ()
+    else putStr ("test failed: " ++ (show permutation) ++ "\n") 
 
--- Test insertion
-testInsertion :: IO ()
-testInsertion = do
-    let tree = insert 25 "new" largerTestTree
-    case find 25 tree of
-        Just "new" -> putStrLn "Passed: Insertion."
-        _          -> putStrLn "Failed: Insertion."
-
-
--- Test find
-testFind :: IO ()
-testFind = do
-    let tree = largerTestTree
-    case find 50 tree of
-        Just "n" -> putStrLn "Passed: Find."
-        _        -> putStrLn "Failed: Find."
-
--- Test deletion
-{-
-testDeletion :: IO ()
-testDeletion = do
-    let tree = delete 10 largerTestTree
-    case find 10 tree of
-        Nothing -> putStrLn "Passed: Deletion."
-        _       -> putStrLn "Failed: Deletion."
--}
-
-runAllTests :: IO ()
-runAllTests = do
-    testInsertion
-    testFind
-    -- testDeletion
-
+main :: IO ()
+main = do
+    let perms = permutations [1..11]
+    mapM_ checkBalance perms 
 
 
 {-
