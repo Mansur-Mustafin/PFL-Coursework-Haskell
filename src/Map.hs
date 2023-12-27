2c008d1fc75423f2b1a215c53fb28c5a9564284d
Mansur <mustafin.mansur02@gmail.com>
2023-12-27 04:33:12 +0000

    v2 AVL-tree

--- src/Map.hs
+++ src/Map.hs
@@ -1,7 +1,7 @@
 module Map (Map, empty, insert, find, map2List, isEmpty) where 
 
 -- TODO add garbage collection
-data Map k v = Empty | Node (k,v) (Map k v) (Map k v) deriving Show
+data Map k v = Empty | Node (k,v) (Map k v) (Map k v) Int deriving Show
 
 -- Crate empty tree
 empty :: Map k v
@@ -16,17 +16,21 @@ isEmpty _ = False
 
 -- Insert or update element in a tree
 insert :: (Ord k) => k -> v -> Map k v -> Map k v
-insert k v Empty = Node (k,v) Empty Empty
-insert k v (Node (k1,v1) l r)
- | k > k1 = balance $ Node (k1,v1) l (insert k v r)
- | k < k1 = balance $ Node (k1,v1) (insert k v l) r
- | otherwise = Node (k,v) l r
+insert k v Empty = Node (k,v) Empty Empty 0
+insert k v (Node (k1,v1) l r bf)
+ | k > k1 = let newRight = insert k v r
+                newBf = (height l) - (height newRight)
+            in balance $ Node (k1,v1) l newRight newBf
+ | k < k1 = let newLeft = insert k v l
+                newBf = (height newLeft) - (height r)
+            in balance $ Node (k1,v1) newLeft r newBf
+ | otherwise = Node (k,v) l r bf
 
 
 -- Find element in a tree 
 find :: (Ord k) => k -> Map k v -> Maybe v
 find k Empty = Nothing
-find k (Node (k1,v1) l r)
+find k (Node (k1,v1) l r _)
  | k < k1 = find k l
  | k > k1 = find k r 
  | otherwise = Just v1 
@@ -35,19 +39,19 @@ find k (Node (k1,v1) l r)
 -- Inorder Traversal
 map2List :: Map k v -> [(k,v)]
 map2List Empty = []
-map2List (Node (k,v) esq dir) = map2List esq ++ [(k,v)] ++ map2List dir
+map2List (Node (k,v) esq dir bf) = map2List esq ++ [(k,v)] ++ map2List dir
 
 
 -- Calculate the height of a tree
 height :: Map k v -> Int 
 height Empty = 0
-height (Node _ esq dir) = 1 + max (height esq) (height dir)
+height (Node _ esq dir bf) = 1 + max (height esq) (height dir)
 
 
 -- Check if a tree is balanced
 isBalanced :: Map k v -> Bool
 isBalanced Empty = True 
-isBalanced (Node (k,v) esq dir) 
+isBalanced (Node (k,v) esq dir bf) 
  | not (isBalanced esq && isBalanced dir) = False
  | abs ((height esq) - (height dir)) > 1 = False
  | otherwise = True 
@@ -55,40 +59,41 @@ isBalanced (Node (k,v) esq dir)
 
 -- Get left subtree 
 leftSubTree :: Map k v -> Map k v
-leftSubTree (Node (k,v) esq dir) = esq
+leftSubTree (Node (k,v) esq dir bf) = esq
 
 
 -- Get right subtree 
 rightSubTree :: Map k v -> Map k v
-rightSubTree (Node (k,v) esq dir) = dir 
+rightSubTree (Node (k,v) esq dir bf) = dir 
 
 
--- Simple Left Rotation of a tree
-leftRotation :: Map k v -> Map k v 
-leftRotation (Node (zk,zv) t1 (Node (yk, yv) t2 x)) 
- = Node (yk, yv) (Node (zk, zv) t1 t2) x 
-leftRotation _ = error "deu merda"
+-- Get Balance Factor
+getBf :: Map k v -> Int 
+getBf (Node _ _ _ bf) = bf
 
 
--- Simple Right Rotation of a tree
-rightRotation :: Map k v -> Map k v 
-rightRotation (Node (zk,zv) (Node (yk, yv) x t3) t4) 
- = Node (yk, yv) x (Node (zk, zv) t3 t4)
-rightRotation _ = error "deu merda"
+-- Left Left Case 
+llRotation :: Map k v -> Map k v
+llRotation (Node (zk, zv) (Node (yk, yv) x t3 ybf) t4 zbf) =
+    Node (yk, yv) x (Node (zk, zv) t3 t4 (zbf - 2)) (ybf - 1)
+llRotation _ = error "LL rotation cannot be applied."
+
+
+-- Right Right Case 
+rrRotation :: Map k v -> Map k v
+rrRotation (Node (zk, zv) t1 (Node (yk, yv) t2 x ybf) zbf) =
+    Node (yk, yv) (Node (zk, zv) t1 t2 (zbf + 2)) x (ybf + 1)
+rrRotation _ = error "RR rotation cannot be applied."
 
 
 -- Balance a tree
 balance :: Map k v -> Map k v 
-balance (Node (k, v) esq dir)
- | balanceIndex == 2  = if height (leftSubTree esq) > height (rightSubTree esq)
-                        then rightRotation (Node (k, v) esq dir)
-                        else rightRotation $ Node (k, v) (leftRotation esq) dir 
- | balanceIndex == -2 = if height (leftSubTree dir) < height (rightSubTree dir)
-                        then leftRotation (Node (k, v) esq dir)
-                        else leftRotation $ Node (k, v) esq (rightRotation dir)
- | otherwise = (Node (k, v) esq dir)
- where 
-  balanceIndex = (height esq) - (height dir)
+balance (Node (k, v) esq dir bf)
+ | bf == 2  && (getBf esq) == 1  = llRotation (Node (k, v) esq dir bf)
+ | bf == -2 && (getBf dir) == -1 = rrRotation (Node (k, v) esq dir bf)
+ | bf == 2  && (getBf esq) == -1 = llRotation $ Node (k, v) (rrRotation esq) dir bf
+ | bf == -2 && (getBf dir) == 1  = rrRotation $ Node (k, v) esq (llRotation dir) bf
+ | otherwise = (Node (k, v) esq dir bf)
 
 
 -- ####################################################################################################################
@@ -113,7 +118,7 @@ muchLargerTestTree = createTree [
     (90, "I"), (100, "J"), (110, "K"), (120, "L"),
     (130, "M"), (140, "N"), (150, "O"), (160, "P"),
     (170, "Q"), (180, "R"), (190, "S"), (200, "T"),
-    (290, "CC"), (300, "DD")
+    (290, "CC"), (300, "DD"), (60, "F"), (320, "FF")
     ]
 
 -- Test insertion
@@ -173,17 +178,3 @@ runAllTests = do
 
 -}
 
-
-{-
-muchLargerTestTree :: Map Int String
-muchLargerTestTree = createTree [
-    (10, "A"), (20, "B"), (30, "C"), (40, "D"),
-    (250, "Y"), (260, "Z"), (270, "AA"), (280, "BB"),
-    (50, "E"), (310, "EE"), (70, "G"), (80, "H"),
-    (210, "U"), (220, "V"), (230, "W"), (240, "X"),
-    (90, "I"), (100, "J"), (110, "K"), (120, "L"),
-    (130, "M"), (140, "N"), (150, "O"), (160, "P"),
-    (170, "Q"), (180, "R"), (190, "S"), (200, "T"),
-    (290, "CC"), (300, "DD"), (60, "F"), (320, "FF")
-    ]
--}
