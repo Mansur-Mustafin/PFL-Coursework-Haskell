c72d6f0fd8f2ba805a3f8bbc3d64bb45e6ea7e21
Mansur <mustafin.mansur02@gmail.com>
2023-12-28 07:00:29 +0000

    added pilha Haddock documentation

--- src/Pilha.hs
+++ src/Pilha.hs
@@ -1,56 +1,82 @@
-module Pilha (Pilha,                                         -- exportar o tipo
-              push, pop, top, get2RightValues, get2LeftValues,  -- e as operações
-              empty, isEmpty) where
+{-|
+Module      : Pilha
+Description : Stack Implementation in Haskell
+Copyright   : Who?
+Maintainer  : who@gmail.com
 
+This module 'Pilha' provides a generic stack (LIFO) data structure.
+It allows users to perform basic operations.
+
+The module also includes specialized functions for handling stacks containing 'Either' values. 
+'get2RightValues' and 'get2LeftValues'.
+-}
+
+module Pilha (
+      -- Export of type
+      Pilha, 
+      -- Export of operations
+      push, pop, top, get2RightValues, get2LeftValues,
+      empty, isEmpty) where
+
+{-|
+    'Pilha' type is represents a Stack. Is a wrapper around a list.
+-}
 data Pilha a = Stk [a] deriving Show
 
+{-|
+    'empty'
+    Description: Yields a empty stack
+-}
 empty :: Pilha a
 empty = Stk []
 
+{-|
+    'isEmpty' 
+    Description: checks if a stack is empty. 
+-}
 isEmpty :: Pilha a -> Bool
 isEmpty (Stk []) = True
 isEmpty (Stk _) = False
 
+{-|
+    'push'
+    Description: adds a new element to the top of the stack.
+-}
 push :: a -> Pilha a -> Pilha a 
 push x (Stk xs) = Stk (x:xs)
 
+{-|
+    'pop'
+    Description: removes the top element from the stack.
+-}
 pop :: Pilha a -> Pilha a
 pop (Stk (_:xs)) = Stk xs
 pop (Stk []) = error "Pilha.pop: empty stack"
 
+{-|
+    'top'
+    Description: returns the top element of the stack.
+-}
 top :: Pilha a -> a
 top (Stk (x:_)) = x
 top (Stk _) = error "Pilha.top: empty stack"
 
+{-|
+    'get2RightValues'
+    Description: retrieves two consecutive 'Right' values from 
+    the top of the stack if available.
+    Return: tuple of the two values and the remaining stack.
+-}
 get2RightValues :: Pilha (Either a b) -> (b, b, Pilha (Either a b))
 get2RightValues (Stk (Right v1:Right v2:xs)) = (v1, v2, Stk xs)
 get2RightValues (Stk _) = error "Run-time error"
 
+{-|
+    'get2LeftValues'
+    Description: retrieves two consecutive 'Left' values from 
+    the top of the stack if available.
+    Return: tuple of the two values and the remaining stack.
+-}
 get2LeftValues :: Pilha (Either a b) -> (a, a, Pilha (Either a b))
 get2LeftValues (Stk (Left v1:Left v2:xs)) = (v1, v2, Stk xs)
 get2LeftValues (Stk _) = error "Run-time error"
-
-
--- Old versions of functions:
-
-{-
-    get2RightValues :: Pilha (Either a b) -> (b, b, Pilha (Either a b))
-    get2RightValues pilha
-     | topIsRight pilha = (v1, v2, pop . pop $ pilha)
-     | otherwise = error "Run-time error" 
-     where 
-      Right v1 = top pilha
-      Right v2 = top (pop pilha)
-
-    topIsRight :: Pilha (Either String Integer) -> Bool
-    topIsRight pilha = isRight (top pilha) && isRight (top (pop pilha))
--}
-
--- One version with top pop ...
-{-
-get2RightValues :: Pilha (Either a b) -> (b, b, Pilha (Either a b))
-get2RightValues pilha = 
-  case (top pilha, top . pop $ pilha) of
-    (Right v1, Right v2) -> (v1, v2, pop . pop $ pilha)
-    _                    -> error "Run-time error"
--}
