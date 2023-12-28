6fb34743cb89d2c2e2224a55332a8ce78b78c162
Mansur <mustafin.mansur02@gmail.com>
2023-12-28 06:33:07 +0000

    refactor code with case-of structure

--- src/Pilha.hs
+++ src/Pilha.hs
@@ -1,10 +1,15 @@
 module Pilha (Pilha,                                         -- exportar o tipo
-              push, pop, top, get2RightInt, get2LeftString,  -- e as operações
-              empty, isEmpty, topIsLeft, topIsRight) where
-
-import Data.Either (isLeft, isRight)
+              push, pop, top, get2RightValues, get2LeftValues,  -- e as operações
+              empty, isEmpty) where
 
 data Pilha a = Stk [a] deriving Show
+
+empty :: Pilha a
+empty = Stk []
+
+isEmpty :: Pilha a -> Bool
+isEmpty (Stk []) = True
+isEmpty (Stk _) = False
 
 push :: a -> Pilha a -> Pilha a 
 push x (Stk xs) = Stk (x:xs)
@@ -15,36 +20,37 @@ pop (Stk []) = error "Pilha.pop: empty stack"
 
 top :: Pilha a -> a
 top (Stk (x:_)) = x
-top _ = error "Pilha.top: empty stack"
+top (Stk _) = error "Pilha.top: empty stack"
 
-empty :: Pilha a
-empty = Stk []
+get2RightValues :: Pilha (Either a b) -> (b, b, Pilha (Either a b))
+get2RightValues (Stk (Right v1:Right v2:xs)) = (v1, v2, Stk xs)
+get2RightValues (Stk _) = error "Run-time error"
 
-isEmpty :: Pilha a -> Bool
-isEmpty (Stk []) = True
-isEmpty (Stk _) = False
-
--- aux functions
-get2RightInt :: Pilha (Either Bool Integer) -> (Integer, Integer, Pilha (Either Bool Integer))
-get2RightInt pilha
- | topIsRight pilha = (v1, v2, pop . pop $ pilha)
- | otherwise = error "Run-time error" 
- where 
-  Right v1 = top pilha
-  Right v2 = top (pop pilha)
+get2LeftValues :: Pilha (Either a b) -> (a, a, Pilha (Either a b))
+get2LeftValues (Stk (Left v1:Left v2:xs)) = (v1, v2, Stk xs)
+get2LeftValues (Stk _) = error "Run-time error"
 
 
-get2LeftString :: Pilha (Either Bool Integer) -> (Bool, Bool, Pilha (Either Bool Integer))
-get2LeftString pilha
- | topIsLeft pilha = (v1, v2, pop . pop $ pilha)
- | otherwise = error "Run-time error" 
- where 
-  Left v1 = top pilha
-  Left v2 = top (pop pilha)
+-- Old versions of functions:
 
+{-
+    get2RightValues :: Pilha (Either a b) -> (b, b, Pilha (Either a b))
+    get2RightValues pilha
+     | topIsRight pilha = (v1, v2, pop . pop $ pilha)
+     | otherwise = error "Run-time error" 
+     where 
+      Right v1 = top pilha
+      Right v2 = top (pop pilha)
 
-topIsLeft :: Pilha (Either Bool Integer) -> Bool
-topIsLeft pilha = isLeft (top pilha) && isLeft (top (pop pilha))
+    topIsRight :: Pilha (Either String Integer) -> Bool
+    topIsRight pilha = isRight (top pilha) && isRight (top (pop pilha))
+-}
 
-topIsRight :: Pilha (Either Bool Integer) -> Bool
-topIsRight pilha = isRight (top pilha) && isRight (top (pop pilha))
+-- One version with top pop ...
+{-
+get2RightValues :: Pilha (Either a b) -> (b, b, Pilha (Either a b))
+get2RightValues pilha = 
+  case (top pilha, top . pop $ pilha) of
+    (Right v1, Right v2) -> (v1, v2, pop . pop $ pilha)
+    _                    -> error "Run-time error"
+-}
